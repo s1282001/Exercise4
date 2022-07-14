b948a2a43ec29ac0e591130333f4511b0c013da4
s1282001 <109281829+s1282001@users.noreply.github.com>
2022-07-14 19:57:25 +0900

    Create headsortails.c

---
+++ headsortails.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<stdlib.h>
+int main() {
+    int random;
+    int hcount=0;
+    int tcount=0;
+    printf("Tossing a coin...\n");
+    for(int i=0;i<3;i++)
+    {
+        printf("Round %d: ",i+1);
+        random=rand()%2;
+        if(random==0){
+        printf("Heads\n");
+        hcount++;}
+        else{
+        printf("Tails\n");
+        tcount++;}
+    }
+    printf("Heads: %d, Tails: %d",hcount,tcount);
+    return 0;
+}
